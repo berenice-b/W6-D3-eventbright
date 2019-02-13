da65f5465ba9cbe1783283cc25cb079fa2a0108b
berenice bourgon <berenice.bourgon@gmail.com>
2019-02-13 13:31:32 +0100

    ajout du authenticate user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: [:secret]
   def index
+    @events = Event.all
+end
+def show
+    @event = Event.find(params[:id])
   end
-
+  def new
+    @event = Event.new
+  end
   def secret
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :user_match, only: [:show]
     def index
         @users = User.all
       end
     
       def show
         @user = User.find(params[:id])
+        @events = Event.where("admin_id=?", params[:id])
       end
     
       def new
         @user = User.new
       end
 
+      def user_match
+      if current_user == User.find(params[:id])
+        redirect_to root_path
+      end
+      end
 end
