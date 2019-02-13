f46f66319bc59d40f4606587fae8dbabcdee6bb8
berenice bourgon <berenice.bourgon@gmail.com>
2019-02-13 11:09:16 +0100

    ajout du dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/secret'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root 'events#index' 
+   root 'home#index' 
    resources :events
    resources :users
 end
